8f43a1a5295b60471e0aad4389f13bd0b5ff28ba
Qinbf <114479602@qq.com>
2018-12-05 23:38:54 +0800

    update bottleneck

---
+++ bottleneck/predict.py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+import os
+import numpy as np
+import re
+from PIL import Image
+
+
+# In[7]:
+
+
+lines = tf.gfile.GFile('output_labels.txt').readlines()
+uid_to_human = {}
+
+# 一行一行读取数据
+for uid,line in enumerate(lines) :
+    #去掉换行符
+    line=line.strip('\n')
+    uid_to_human[uid] = line
+
+# 分类编号变成描述
+def id_to_string(node_id):
+    if node_id not in uid_to_human:
+        return ''
+    return uid_to_human[node_id]
+
+
+# 创建一个图来存放训练好的模型
+with tf.gfile.FastGFile('output_graph.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+
+with tf.Session() as sess:
+    # final_result为输出tensor的名字
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    # 遍历目录
+    for root,dirs,files in os.walk('test_images/'):
+        for file in files:
+            # 载入图片
+            image_data = tf.gfile.FastGFile(os.path.join(root,file), 'rb').read()
+            # 把图像数据传入模型获得模型输出结果
+            predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': image_data})
+            # 把结果转为1维数据
+            predictions = np.squeeze(predictions)
+            # 打印图片路径及名称
+            image_path = os.path.join(root,file)
+            print(image_path)
+
+            # 排序
+            top_k = predictions.argsort()[::-1]
+            for node_id in top_k:     
+                # 获取分类名称
+                human_string = id_to_string(node_id)
+                # 获取该分类的置信度
+                score = predictions[node_id]
+                print('%s (score = %.5f)' % (human_string, score))
+            print()
+
---
+++ bottleneck/retrain.sh
@@ -0,0 +1,1 @@
+python retrain.py --bottleneck_dir bottleneck --how_many_training_steps 200 --model_dir inception_model --output_graph output_graph.pb --output_labels output_labels.txt --image_dir data
